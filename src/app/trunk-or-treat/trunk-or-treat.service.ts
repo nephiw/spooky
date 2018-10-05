fd1044cf48a0084f690355a152b02202aef74b9b
Nephi Wright <nephiw@gmail.com>
2018-10-04 23:13:03 -0600

    Validate email addresses and make sanitized email the database key

--- src/app/trunk-or-treat/trunk-or-treat.service.ts
+++ src/app/trunk-or-treat/trunk-or-treat.service.ts
@@ -4,16 +4,24 @@ import { Contact } from './contact';
 
 @Injectable()
 export class TrunkOrTreatService {
+  private readonly InvalidStringRegEx = /\./g;
 
   constructor(private db: AngularFireDatabase) { }
 
   public createNewContact(data): AngularFireObject<Contact> {
     const contact = new Contact(data);
-    const contactKey = this.db.list('contacts').push(contact).key;
+    const contactKey = this.sanitizeEmail(contact.emailAddress);
+    const contacts = this.db.object('/contacts');
+    contacts.update({ [ contactKey ]: contact });
+
     return this.db.object(`/contact/${ contactKey }`);
   }
 
   public updateContact(contact: AngularFireObject<Contact>, data: any) {
     return contact.update(data);
+  }
+
+  private sanitizeEmail(email: string): string {
+    return email.replace(this.InvalidStringRegEx, '_');
   }
 }
