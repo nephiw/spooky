a4461c8c8783cf467065d1e36216a40fd2fe0559
Nephi Wright <nephiw@gmail.com>
2019-10-17 17:06:51 -0600

    Provide all lower case re-directs because they were in analytics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,18 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { AuthenticationPageComponent } from 'common/authentication';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
   { path: 'authentication',  component: AuthenticationPageComponent },
   { path: 'admin',           loadChildren: './admin/admin.module#AdminModule' },
   { path: 'trunkOrTreat',    loadChildren: './trunk-or-treat/trunk-or-treat.module#TrunkOrTreatModule' },
+  { path: 'trunkortreat',    redirectTo: 'trunkOrTreat' },
   { path: 'booYourNeighbor', loadChildren: './boo-your-neighbor/boo-your-neighbor.module#BooYourNeighborModule' },
+  { path: 'booyourneighbor', redirectTo: 'booYourNeighbor' },
   { path: 'houseDecoration', loadChildren: './house-decoration/house-decoration.module#HouseDecorationModule' },
-  { path: 'houseVote',       loadChildren: './house-vote/house-vote.module#HouseVoteModule' }
+  { path: 'housedecoration', redirectTo: 'houseDecoration' },
+  { path: 'houseVote',       loadChildren: './house-vote/house-vote.module#HouseVoteModule' },
+  { path: 'housevote',       redirectTo: 'houseVote' },
+  { path: '', component: HomePageComponent },
+  { path: '**', redirectTo: '/'}
 ];
 
 @NgModule({
